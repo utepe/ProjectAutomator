5a0fcaefa3de807293e378ca151dc96d87288609
Uygur Tepe <uygur.t10@gmail.com>
2019-11-24 21:46:26 -0500

    streamlined automation of README.md

--- autoProject.bat
+++ autoProject.bat
@@ -3,9 +3,9 @@
     cd users\uygur\Documents\Code\MyProjects\
     mkdir %1
     cd %1
+    echo "# %1" >> README.md
     git init
     git remote add origin https://utepe@github.com/utepe/%1.git
-    echo  >> README.md
     git add .
     git commit -m "Initial commit"
     git push origin master
